3b2f325aa0d73d704079b12c1530c5702a92e9ce
nkoshkina <nkoshkina@gmail.com>
2024-11-23 19:07:55 +0300

    Added testRedirect()

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -47,5 +47,21 @@ public class HelloWorldTest {
             System.out.println(message);
         }
     }
+
+    @Test
+    public void testRedirect() {
+
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
+    }
+
 }
 
