920f29e80d40f6d67826a4a465ae2648aca22ec9
nkoshkina <nkoshkina@gmail.com>
2024-11-23 17:15:28 +0300

    Added testGetFullJson, testGetJsonValue

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,21 +1,51 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-public class HelloWorldTest {
+import java.util.HashMap;
+import java.util.Map;
 
+public class HelloWorldTest {
     @Test
     public void testHelloWorld() {
 
         System.out.println("Hello from Nadezhda");
     }
-
     @Test
     public void testGet() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Tom");
 
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+
+        String answer = response.get("answer");
+        System.out.println(answer);
+    }
+    @Test
+    public void testGetFullJson() {
         Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
+                .get("https://playground.learnqa.ru/api/get_json_homework")
                 .andReturn();
         response.prettyPrint();
     }
+    @Test
+    public void testGetJsonValue() {
+
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        String message = response.get("messages.message[1]");
+        if (message == null) {
+            System.out.println("Value of 2nd message or 2nd message key  is absent");
+        } else {
+            System.out.println(message);
+        }
+    }
 }
+
